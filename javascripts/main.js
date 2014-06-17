d8eb956da383e2cc431ca4a84f285083a74f5cb1
SerjNarbut <serjnarbut@gmail.com>
2014-06-18 03:59:42 +0400

    site - new test program

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,6 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function game(){
 
@@ -6,8 +9,9 @@ function game(){
     gameApp.
         constant("WIDTH", 800) /* установка константы */
         .constant("HEIGHT", 600)
-        .constant("MAX_DIST", 100)
-        .constant("MAX_DIST_GOAL", 100)
+        .constant("MAX_DIST", 20)
+        .constant("MAX_DIST_GOAL", 7)
+        .constant("ANGLE",25)
         .service("Stage", function(){ /* создание сервиса*/
             var stage = new PIXI.Stage(0x888888, true);
             return{
@@ -44,144 +48,129 @@ function game(){
                 }
             }
         })
+        .service("AngleConverter", function(){
+            return{
+                toDegree: function(radian){
+                    return radian * 180 / Math.PI;
+                },
+                toRadian: function(degree){
+                    return degree * Math.PI / 180;
+                }
+            }
+        })
+        .service("UnitGenerator", function(ANGLE, AngleConverter, SpriteFactory){
+            var current = 1;
+            var center = new steerjs.Vector(400,300);
+            var r = 100;
+            function coordinate(angle){
+                return new steerjs.Vector(Math.floor(center.x +r * Math.sin(angle)), Math.floor(center.y + r*Math.cos(angle)) );
+            }
+            return{
+                generateNewUnit: function(){
+                    var angle = AngleConverter.toRadian(current * ANGLE);
+                    var goalAngle = AngleConverter.toRadian(current * ANGLE + 180);
+                    current++;
+                    return{
+                        acceleration: steerjs.Vector.zero(),
+                        velocity: steerjs.Vector.zero(),
+                        maxSpeed: getRandomInt(1,5),
+                        location: coordinate(angle),
+                        sprite: SpriteFactory.getSprite("monster", "images/dorver-idle0.png"),
+                        isReachGoal: false,
+                        goal: coordinate(goalAngle),
+
+                        update: function(force){
+                            if(force != null){
+                                this.acceleration.add(force);
+                                this.velocity.add(this.acceleration);
+                                this.velocity.limit(this.maxSpeed);
+                                this.location.add(this.velocity);
+                                this.acceleration = steerjs.Vector.zero();
+                            }
+                            this.sprite.position.x = this.location.x;
+                            this.sprite.position.y = this.location.y;
+                        }
+                    }
+                }
+            }
+
+        })
         .algorithm("arrival", steerjs.arrival).algorithm("separate",steerjs.separate) /* регистрация алгоритмов*/
         .handlerConfig(function($handler){  /*  конфигурация обработчика событий */
             $handler.on("hasGoal.simple",{
                 "algo":[
-                    {"name": "arrival", "params":{alfa: 1, closestsRadius: 100} }
+                    {"name": "arrival", "params":{alfa: 1, closestsRadius: 50} }
                 ]
             });
 
             $handler.on("hasGoal.hard",{
                 "algo":[
-                    {"name": "arrival", "params":{alfa: 1, closestsRadius: 200} },
-                    {name: "separate", "params": {alfa: 15, separateRadius: 170}}
+                    {"name": "arrival", "params":{alfa: 0.4, closestsRadius: 50} },
+                    {name: "separate", "params": {alfa: 15, separateRadius: 45}}
                 ]
             });
 
             $handler.on("reachGoal",{
                 "algo":[
-                    {name: "separate", "params": {alfa: 2, separateRadius: 170}}
+                    {name: "separate", "params": {alfa: 10, separateRadius: 45}}
                 ]
             });
         })
         .boot();
 
-    gameApp.run(function(Stage, Renderer, SpriteFactory, MAX_DIST, MAX_DIST_GOAL){
+    gameApp.run(function(Stage, Renderer, UnitGenerator, MAX_DIST, MAX_DIST_GOAL){
        // document.body.appendChild(Renderer.getRender().view);
         $("#testGame").append(Renderer.getRender().view);
-        var monsters = [];
-        var units = [
-            {
-                velocity: steerjs.Vector.zero(),
-                acceleration: steerjs.Vector.zero(),
-                location: new steerjs.Vector(100,100),
-                maxSpeed: 3,
-                reach: false
-            } ,
-            {
-                velocity: steerjs.Vector.zero(),
-                acceleration: steerjs.Vector.zero(),
-                location: new steerjs.Vector(200,200),
-                maxSpeed: 4,
-                reach: false
-            } ,
-            {
-                velocity: steerjs.Vector.zero(),
-                acceleration: steerjs.Vector.zero(),
-                location: new steerjs.Vector(300,100),
-                maxSpeed: 3,
-                reach: false
-            },
-
-            {
-                velocity: steerjs.Vector.zero(),
-                acceleration: steerjs.Vector.zero(),
-                location: new steerjs.Vector(800,600),
-                maxSpeed: 3,
-                reach: false
-            }
-        ];
-        monsters[0] = SpriteFactory.getSprite("monster", "images/dorver-idle0.png");
-        monsters[1] = SpriteFactory.getSprite("monster", "images/dorver-idle0.png");
-        monsters[2] = SpriteFactory.getSprite("monster", "images/dorver-idle0.png");
-        monsters[3] = SpriteFactory.getSprite("monster", "images/dorver-idle0.png");
+        var units = [];
 
 
-        monsters[0] .position.x = 100;
-        monsters[0] .position.y = 100;
-        monsters[1] .position.x = 200;
-        monsters[1] .position.y = 200;
-        monsters[2] .position.x = 300;
-        monsters[2] .position.y = 100;
-        monsters[3] .position.x = 800;
-        monsters[3] .position.y = 600;
+        var count  = Math.floor(360 / 25);
+        for(var i = 0; i < count; i++){
+            units.push(UnitGenerator.generateNewUnit());
+            Stage.getStage().addChild(units[i].sprite);
+        }
 
-        Stage.getStage().addChild(monsters[0]);
-        Stage.getStage().addChild(monsters[1]);
-        Stage.getStage().addChild(monsters[2]);
-        Stage.getStage().addChild(monsters[3]);
-
-        var goal = null;
+        var go = false;
         Stage.getStage().click  = function(mouseData){
-            var coord = mouseData.getLocalPosition(Stage.getStage());
-            goal = new steerjs.Vector(coord.x, coord.y);
-            for(var i = 0; i < units.length; i++) {
-                units[i].reach = false;
-            }
+            go = true;
         };
 
         requestAnimFrame(animate);
 
 
         function animate(){
-            var goalReached = false;
+
             for(var i = 0; i < units.length; i++){
-                var force = null;
-                if(goal != null && !units[i].reach){
-                    force = gameApp.handler().fire("hasGoal.hard", {unit: units[i], goal: goal, units: units });
-                }
-                if(units[i].reach){
-                    force = gameApp.handler().fire("reachGoal", {unit: units[i], units: units });
-                }
-
-                if(force != null){
-                    units[i].acceleration.add(force);
-                    units[i].velocity.add(units[i].acceleration);
-                    units[i].velocity.limit(units[i].maxSpeed);
-                    units[i].location.add(units[i].velocity);
-                    units[i].acceleration = steerjs.Vector.zero();
-                    monsters[i].position.x = units[i].location.x;
-                    monsters[i].position.y = units[i].location.y;
-                }
-
-
-                if(goal != null && !units[i].reach){
-                    units[i].reach = steerjs.Vector.distance(goal,units[i].location) < MAX_DIST_GOAL;
-                    if(units[i].reach){
-                        units[i].velocity = steerjs.Vector.zero();
+                if(go){
+                    var force = null;
+                    units[i].isReachGoal = steerjs.Vector.distance(units[i].goal,units[i].location) < MAX_DIST_GOAL;
+                    if(units[i].goal != null && !units[i].isReachGoal){
+                        force = gameApp.handler().fire("hasGoal.hard", {unit: units[i], goal: units[i].goal, units: units });
                     }
-                    goalReached &= units[i].reach;
-                    if(goalReached){
-                        goal = null;
-                        goalReached = false;
+                    if(units[i].isReachGoal){
+                        force = gameApp.handler().fire("reachGoal", {unit: units[i], units: units });
                     }
-                }
 
-                if( units[i].reach){
-                    var d = false;
-                    for(var j = 0; j < units.length;j++){
-                        if(units[i] == units[j]) continue;
-                        var dist = steerjs.Vector.distance(units[i].location, units[j].location);
-                        if(dist < MAX_DIST){
-                            d = true;
-                            break;
+                    units[i].update(force);
+                    units[i].isReachGoal = steerjs.Vector.distance(units[i].goal,units[i].location) < MAX_DIST_GOAL;
+                    if( units[i].isReachGoal){
+                        var d = false;
+                        for(var j = 0; j < units.length;j++){
+                            if(units[i] == units[j]) continue;
+                            var dist = steerjs.Vector.distance(units[i].location, units[j].location);
+                            if(dist < MAX_DIST){
+                                d = true;
+                                break;
+                            }
+                        }
+                        if(!d){
+                            units[i].velocity = steerjs.Vector.zero();
                         }
                     }
-                    if(!d){
-                        units[i].velocity = steerjs.Vector.zero();
-                    }
+                }else{
+                    units[i].update(null);
                 }
+
             }
             Renderer.getRender().render(Stage.getStage());
             requestAnimFrame(animate);
